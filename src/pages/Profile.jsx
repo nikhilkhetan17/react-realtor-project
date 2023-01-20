63ec3c546d59068058f6dd736bc1dd08ce8f5990
nikhilkhetan17 <iamnikhilkhetan@gmail.com>
2023-01-20 21:08:49 +0530

    created private route and custom hook for protacting profile page and logout functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,61 @@
-import React from "react";
+import { getAuth } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+  const { name, email } = formData;
+
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
+  }
+  return (
+    <>
+      <section className="flex items-center  flex-col">
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            {/* Name imput */}
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white rounded border-gray-300 transition ease-in-out"
+            />
+            {/* Email imput */}
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white rounded border-gray-300 transition ease-in-out my-4"
+            />
+
+            <div className="flex justify-between whitespace-nowrap sm:text-lg mb-4">
+              <p>
+                Do you want to change your name?
+                <span className="ml-2 text-red-600 hover:text-red-700 cursor-pointer transition ease-in-out font-medium">
+                  Edit
+                </span>
+              </p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-800 cursor-pointer transition ease-in-out font-medium"
+              >
+                Sign out
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
